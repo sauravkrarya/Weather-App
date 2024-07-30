003aae2c4266db6428455ddc93f2b80fbd149e02
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-07-31 00:09:52 +0530

    update

--- weathercheck/src/Weather.js
+++ weathercheck/src/Weather.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function Weather() {
   const[city, setCity]=useState();
+  const handleCityChange =(event) => {
+    setCity(event.target.value)
+  }
   return (
     
-    <div classname='weather-container'>
+    <div className='weather-container'>
       <input type='text' placeholder='Enter the City Name' value={city} onChange={handleCityChange} />
     </div>
   )
